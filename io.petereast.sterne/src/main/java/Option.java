0fc986784e3bd7f2914ef84e5e2e4d24e48e413f
Peter East <petereast@pm.me>
2019-01-18 20:05:29 +0000

    Add an optional helper class for sweet sweet functional goodness.

---
+++ io.petereast.sterne/src/main/java/Option.java
@@ -0,0 +1,57 @@
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+
+public class Option<T> {
+    private T thing;
+    private boolean isSome = false;
+
+    public static Option<T> of(T thing) {
+        if (thing == null) {
+            return new Option(false, null);
+        } else {
+            return new Option(true, thing);
+        }
+    }
+
+    public Option(boolean isSome, T thing) {
+        this.isSome = isSome;
+        this.thing = thing;
+    }
+
+    public <O> Option<O> map(Function<T, O> fn) {
+
+        if (this.isSome) {
+            return new Option(true, fn.apply(this.thing));
+        } else {
+            return new Option(false, null);
+        }
+    }
+
+    public T get() {
+        if (!this.isSome) {
+            throw new NoSuchElementException();
+        } else {
+            return this.thing;
+        }
+    }
+
+    public T getOrElse(T other) {
+        if (!this.isSome){
+            return other;
+        } else {
+            return this.thing;
+        }
+    }
+
+    public T getOrNull() {
+        return this.thing;
+    }
+
+    public <O> Option<O> replace(O other) {
+        if (this.isSome) {
+            return new Option(true ,this.thing);
+        } else {
+            return Option.of(other);
+        }
+    }
+}
